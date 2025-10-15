6578cb405b51c57d3382fd8b9614ac5ab6a4be7e
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-10-15 11:16:38 +0545

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">5 Min React Pizza</Link>
+      <p>Hello world </p>
+    </header>
+  );
+}
+
+export default Header;
