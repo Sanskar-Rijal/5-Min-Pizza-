d1a01aa43fa304abd221f79f0e27281991f44e0d
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-10-15 12:35:24 +0545

    fetching menu and showing error if fails

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as MenuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import Cart from "./features/cart/Cart";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />, //it does not have a path, so react understands it as a layout route
+    errorElement: <Error />, //if an error comes then it will display this component
     children: [
       {
         path: "/",
@@ -17,6 +19,8 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        errorElement: <Error />,
+        loader: MenuLoader,
       },
       {
         path: "/cart",
